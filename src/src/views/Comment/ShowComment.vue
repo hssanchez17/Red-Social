42365bc0e809994a4b5d410dc610c8b2e81dee10
hssanchez17 <hssanchez.17@est.ucab.edu.ve>
2020-01-28 09:56:06 -0430

    Ya actualiza el comentario

--- src/src/views/Comment/ShowComment.vue
+++ src/src/views/Comment/ShowComment.vue
@@ -11,16 +11,36 @@
                 <div class="row">
 
                 	<a :href="`/user/show/${comment.userId}`" class="col text-center">
-                    	<img src="https://scontent-mia3-2.xx.fbcdn.net/v/t31.0-8/s960x960/15800243_1378924182125785_3186974028727972417_o.jpg?_nc_cat=105&_nc_ohc=nDhh6znLXUkAX8S7OK3&_nc_ht=scontent-mia3-2.xx&_nc_tp=1002&oh=bc7952c964fb005af9f9b28c42b295b8&oe=5EDB22D5" id="images" width="120px" height="120px"/>
-                  	</a>
+                    <img src="https://scontent-mia3-2.xx.fbcdn.net/v/t31.0-8/s960x960/15800243_1378924182125785_3186974028727972417_o.jpg?_nc_cat=105&_nc_ohc=nDhh6znLXUkAX8S7OK3&_nc_ht=scontent-mia3-2.xx&_nc_tp=1002&oh=bc7952c964fb005af9f9b28c42b295b8&oe=5EDB22D5" id="images" width="120px" height="120px"/>
+                  </a>
                  
-                  <blockquote class="col">
+                  <blockquote class="col" v-if="!permissionToUpdate">
                     <p class="lead">{{comment.comment}}</p>
                     <footer class="blockquote-footer">{{email}}</footer>
                   </blockquote>
 
-                   <button class="btn btn-info" id="btn-toggle-comment">  Update</button>
+                  <div class="col" v-if="permissionToUpdate">
+                    <textarea placeholder="Descripcion de su comentario" v-model="commentEdited.comment"></textarea> 
+                    
+                  </div>
+
+
+                  <div v-if="commentOwner">
+                                      <div v-if='!commentOwner'>
+                   <button class="btn btn-info" id="btn-toggle-comment" @click="mostrarUpdatedCommet()">  Update</button>
                    <button class="btn btn-danger" id="btn-toggle-comment">  Delete</button>
+                  </div>
+
+                  <div v-if='commentOwner'>
+                   <button class="btn btn-info" id="btn-toggle-comment" @click="updateComment()">  Cambiar</button>
+                   <button class="btn btn-danger" id="btn-toggle-comment" @click="ocultarUpdateComment()">  Cancelar</button>
+                    
+
+                  </div>
+                  </div>
+
+
+
                 </div>
               </li>
             </ul>
@@ -45,6 +65,9 @@ export default {
       comment:{},
       email:'',
       id:this.$route.params.id,
+      commentOwner:'',
+      commentEdited:{comment:''},
+      permissionToUpdate:false
 
     }
   },
@@ -57,12 +80,37 @@ export default {
       getComment(){
         this.axios.get(`comment/show2/${this.id}`,{ credentials: true })
         .then((response) => {
-          this.comment= response.data[0];
+          //console.log(response.data)
+          this.comment= response.data.comment[0];
           this.email=this.comment.user.email
-         
+          this.commentOwner=response.data.commentOwner
+          console.log(this.commentOwner)
         })
         .catch((e)=>{
           console.log('error' + e);
+        })
+      },
+
+      mostrarUpdatedCommet(){
+        this.commentEdited.comment=this.comment.comment
+        this.commentOwner=true
+        this.permissionToUpdate=true
+      },
+
+      ocultarUpdateComment(){
+        this.commentOwner=false
+        this.permissionToUpdate=false
+      },
+
+      updateComment(){
+       this.axios.put(`comment/update/${this.id}`,this.commentEdited)
+        .then((response) => {
+          alert('Se actualizo la informacion de el comentario correctamente')
+          this.permissionToUpdate=false
+          console.log()
+        })
+        .catch( e => {
+          console.log(e.response.data.error)
         })
       }
     }
